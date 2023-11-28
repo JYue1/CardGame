f95839854c9e7dfee6ad121f626e94503efb7c03
JamesYue1 <james.yue@menloschool.org>
2023-11-28 13:45:05 -0800

    Finished Card Class and implemented the three other classes

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class Card {
+public class Card {
+    private String ranks;
+    private String suits;
+    private int values;
+
+    public Card(String ranks, String suits, int values) {
+        this.ranks = ranks;
+        this.suits = suits;
+        this.values = values;
+    }
+
+    public String getRanks() {
+        return ranks;
+    }
+
+    public void setRanks(String ranks) {
+        this.ranks = ranks;
+    }
+
+    public String getSuits() {
+        return suits;
+    }
+
+    public void setSuits(String suits) {
+        this.suits = suits;
+    }
+
+    public int getValues() {
+        return values;
+    }
+
+    public void setValues(int values) {
+        this.values = values;
+    }
+
+    public String toString(){
+        return ranks + " of " + suits;
+    }
 }
