8d9b3f610a8abbd767ef7a31735dada55cad25eb
JamesYue1 <james.yue@menloschool.org>
2023-12-07 13:45:02 -0800

    Finished isEmpty, getCardsLeft method and finishing the constructor.

--- src/Deck.java
+++ src/Deck.java
@@ -1,2 +1,52 @@
+import java.util.ArrayList;
+import java.lang.Math;
 public class Deck {
+    // Math.random you want to multiply by a variable because the amount of cards decreases when you deal them out
+    private ArrayList<Card> cards;
+    private int cardsLeft;
+
+    public Deck(String[] ranks, String[] suits, int[] values) {
+        cards = new ArrayList<>();
+        for (String ranks = i; i < 10; i++) {
+            for (String suits = i; i < 4; i++) {
+                Card add = new Card(ranks, suits, )
+            }
+        }
+
+        {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10"}
+        {"Hearts", "Clubs", "Diamonds", "Spades"}
+        {1,2,3,4,5,6,7,8,9,10}
+        // use the shuffle method inside of here
+        cardsLeft = cards.size();
+
+
+    }
+
+    public boolean isEmpty() {
+        return (cardsLeft == 0);
+    }
+
+    public int getCardsLeft() {
+        return cardsLeft;
+    }
+
+    public card deal(){
+        if isEmpy
+
+    }
+
+    public void shuffle() {
+        // This is where you include math.random
+    }
 }
+
+
+
+
+
+
+
+
+
+
+// }
